e48810287c5075fe50b312f8edd9df684928dfe4
James <jamesddale0@gmail.com>
2022-04-09 20:13:52 -0600

    JavaScript implemented

--- calc_func.js
+++ calc_func.js
@@ -1,3 +1,17 @@
-let display = document.getElementById('display');
+var a = document.getElementById("device_list");
+var b = document.getElementById("stream_list");
+var c = document.getElementById("download_list");
 
-let 
+function deviceDropDown(){
+var devNum = a.value;
+var strNum = b.value;
+var dowNum = c.value;
+console.log(devNum, strNum, dowNum);
+
+var mbpsTotal = Number(devNum) + Number(strNum);
+var capTotal = dowNum;
+
+document.getElementById("display").innerHTML = 'Based on your selection, you should consider at least ' + mbpsTotal + 'mbps. Additionally, you should look for a data cap that is no less than ' +capTotal + '.'
+}
+
+
